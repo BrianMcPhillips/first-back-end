fcb9556295c1d45f7c5addab8dae7cd05486fe50
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-11 22:50:31 -0700

    almost working

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
 const port = process.env.PORT || 3000;
-const weatherData = require('./data/weather.js');
-const geoData = require('./data/geo.js');
+
+const express = require('express');
+const request = require('superagent');
+const cors = require('cors');
 
 
+const app = express();
 app.use(cors());
-
 app.use(express.static('public'));
 
-function getLatLong(cityName) {
-    const city = geoData[0];
+
+const {
+    GEOCODE_API_KEY,
+    WEATHER_API_KEY
+
+} = process.env;
+
+ async function getLatLong(cityName) {
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+    const city = response.body[0];
 
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
-        longitude: city.lon,
+        longitute: city.lon
     };
-}
+ }
 
-function getWeather(lat, lon) {
-    const data = weatherData.data;
+ app.get('/location', async(req, res) => {
+    try {
+        const userInput = req.query.search;
+    
+        const mungedData = await getLatLong(userInput);
+        res.json(mungedData);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+
+
+
+
+
+async function getWeather(lat, lon) {
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
+
+    let data = response.body.data;
+
+    data = data.slice(0, 8)
 
     const forecastArray = data.map((weatherItem) => {
         return {
@@ -30,31 +57,25 @@ function getWeather(lat, lon) {
             time: new Date(weatherItem.ts * 1000)
         };
     });
+    return forecastArray;
 }
-
-
-app.get('/location', (req, res) => {
-    try {
-    const userInput = req.query.search;
-    
-      const mungedData = getLatLong(userInput);
-      res.json(mungedData);
-    } catch (e) {
-        res.status(500).json({error: e.message});
-    }
-});
 
 app.get('/weather', (req, res) => {
     try {
-    const userLat = req.query.latitude;
-    const userLon = req.query.longitude;
+        const userLat = req.query.latitude;
+        const userLon = req.query.longitude;
     
-      const mungedData = getWeather(userLat, userLon);
-      res.json(mungedData);
+        const mungedData = getWeather(userLat, userLon);
+        res.json(mungedData);
     } catch (e) {
-        res.status(500).json({error: e.message});
+        res.status(500).json({ error: e.message });
     }
-});
+}); 
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
