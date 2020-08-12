6d3722d92b5e9d65a3e25f02b330e357844fe410
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-12 14:02:38 -0700

    working map and weather

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const {
  async function getLatLong(cityName) {
     const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
     const city = response.body[0];
-
+    console.log('FIND THIS CONSOLE LOG', city);
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
-        longitute: city.lon
+        longitude: city.lon
     };
  }
 
@@ -45,7 +45,8 @@ const {
 
 
 async function getWeather(lat, lon) {
-    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
+    console.log(lat, lon, WEATHER_API_KEY)
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
 
     let data = response.body.data;
 
@@ -60,12 +61,12 @@ async function getWeather(lat, lon) {
     return forecastArray;
 }
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async (req, res) => {
     try {
         const userLat = req.query.latitude;
         const userLon = req.query.longitude;
     
-        const mungedData = getWeather(userLat, userLon);
+        const mungedData = await getWeather(userLat, userLon);
         res.json(mungedData);
     } catch (e) {
         res.status(500).json({ error: e.message });
